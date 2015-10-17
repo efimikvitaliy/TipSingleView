2f4252b614e202c53442bc0e50332136da410c3b
efimikvitaliy <efimikvitaliy@Mac-Admin.local>
2015-10-17 19:49:07 +0300

    Initial Commit

---
+++ TipSingleViewTests/TipSingleViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  TipSingleViewTests.swift
+//  TipSingleViewTests
+//
+//  Created by efimikvitaliy on 17.10.15.
+//  Copyright (c) 2015 DreamTeam. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TipSingleViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
